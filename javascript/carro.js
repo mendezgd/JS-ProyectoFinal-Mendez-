ee5288f5c6e80b67ff222cd55f737eb9db3697b4
gmendez <mendezgd88@gmail.com>
2023-05-10 14:02:27 -0300

    first commit

---
+++ javascript/carro.js
@@ -0,0 +1,81 @@
+// declaraciones //
+let local = localStorage.getItem("carrito");
+let carrito = JSON.parse(local);
+let arrCarrito = [];
+// checkeo si la variable en el storage esta vacia, porque sino da error //
+if (carrito) {
+    arrCarrito = carrito.items
+
+}
+//funcion crea cartas en carro //
+function itemHtml(el) {
+    cardItem = `
+            <div class="card m-1">
+                <div class="row g-0">
+                    <div class="col-md-1">
+                        <img src="../media/${el.img}" class="img-fluid rounded-start" alt="...">
+                    </div>
+                    <div class="col-md-10">
+                        <div class="card-body">
+                            <h5 class="card-title">${el.nombre}</h5>
+                            <p class="card-text">${el.descripcion}</p>
+                            
+                        </div>
+                    </div>
+                    <div class="col-md-1">
+                        <p class="card-text text-center pt-4">$${el.precio}</p>
+                        <button value="${el.id}" class="btn btn-danger borrar">Borrar</button>
+                    </div>
+                </div>
+            </div>`;
+    return cardItem;
+}
+
+// evento del boton borrar //
+eventoBoton = function (e) {
+    arrCarrito = removerId(arrCarrito, e.target.value);
+    let carrito = { items: arrCarrito };
+    localStorage.setItem("carrito", JSON.stringify(carrito));
+    Swal.fire({
+        titleText: 'item eliminado!',
+        icon: 'error',
+        background: '#F1F5F2',
+    })
+    refrescarItems();
+};
+
+// elimino de la lista por ID //
+const removerId = (arr, filtro) => {
+    const buscar = arr.filter((el) => {
+        return el.id != filtro
+    })
+    return buscar
+}
+
+// refresca lista de items en carrito y recalcula el total //
+function refrescarItems() {
+    let carroItems = document.getElementById('carro_items');
+    let total = 0;
+    let campoTotal = document.getElementById('total');
+    arrCarrito.forEach(x => {
+        total += x.precio
+    });
+    campoTotal.innerHTML = total
+    carroItems.innerHTML = "";
+    for (const i of arrCarrito) {
+
+        carroItems.innerHTML += itemHtml(i);
+
+    }
+    agregarListener();
+}
+
+// agrega listeners a todos los botones //
+function agregarListener() {
+    let botonesBorrar = document.getElementsByClassName('borrar');
+    for (const boton of botonesBorrar) {
+        boton.addEventListener('click', eventoBoton);
+    }
+}
+
+refrescarItems();
---
+++ javascript/script.js
@@ -0,0 +1,110 @@
+//declaraciones//
+const arrProdFijo = [
+    { id: 1, nombre: "Ak47", stock: 10, precio: 2500, img: "prod1.jpg", descripcion: "El AK-47 es un fusil de asalto soviético diseñado por Mijaíl Kaláshnikov en 1947" },
+    { id: 2, nombre: "M4A1", stock: 5, precio: 3100, img: "asalto1.jpg", descripcion: "La M4A1 es una carabina de asalto estadounidense que utiliza munición 5,56." },
+    { id: 3, nombre: "Desert Eagle", stock: 22, precio: 700, img: "prod3.jpg", descripcion: "La Desert Eagle es una pistola semiautomática capacidad para disparar cartucho .50 Action Express." },
+    { id: 4, nombre: "Sniper AWP", stock: 10, precio: 5700, img: "awp2.jpg", descripcion: "El AWP es un rifle de francotirador de cerrojo que utiliza munición calibre .338" },
+    { id: 5, nombre: "Chaleco anti-balas", stock: 50, precio: 1000, img: "chaleco1.jpg", descripcion: "El Kevlar es un material sintético resistente y ligero que se utiliza en la fabricación de chalecos antibalas" },
+    { id: 6, nombre: "Escopeta", stock: 40, precio: 1200, img: "escopetas1.jpg", descripcion: "Las escopetas están diseñadas para descargar varios proyectiles, como perdigones o postas, en cada disparo." },
+    { id: 7, nombre: "Optica", stock: 300, precio: 150, img: "opticas1.jpg", descripcion: "Los visores son dispositivos ópticos que se acoplan al arma para mejorar la precisión del disparo." },
+    { id: 8, nombre: "Granada", stock: 22, precio: 400, img: "granadas1.jpg", descripcion: "Estas armas están diseñadas para ser arrojadas desde distancias cortas y causar daño en un radio considerable." },
+];
+
+let arrProdFiltrado = [];
+let arrCarrito = [];
+const iva = 1.21
+let total = 0
+let local = localStorage.getItem("carrito");
+let carrito = JSON.parse(local);
+if (carrito) {
+    if (carrito.items) {
+        arrCarrito = carrito.items
+    };
+}
+
+// filtrado de nombre //
+const buscarNombre = (arr, filtro) => {
+    const buscar = arr.filter((el) => {
+        let minus = el.nombre.toLowerCase();
+        return minus.includes(filtro.toLowerCase());
+    })
+    return buscar
+}
+
+// get y query html //
+const productosDin = document.getElementById('productosDin');
+const inputBuscar = document.querySelector('input[type="search"]');
+const botonesAgregar = document.getElementsByClassName('agregar');
+const listaCarrito = document.getElementById('listaCarrito');
+
+// funcion crea cards //
+function crearCard(el) {
+    let htmlCard = `
+                <div class="col">
+                <div class="card" style="width: 18rem;">
+                    <img src="./media/${el.img}" alt="${el.nombre}" class="card-img-top" width="288px" height="250px">
+                    <div class="card-body">
+                        <h5 class="card-title">${el.nombre}</h5>
+                        <p class="card-text">${el.descripcion}</p>
+                        <button value="${el.id}" class="btn btn-primary agregar" data-bs-toggle="button">Agregar al carro</button>
+                        <p>Precio: $${el.precio}</p>
+                    </div>
+                </div>
+            </div>`;
+    return htmlCard;
+}
+
+// buscador //
+showInput = addEventListener('input', () => {
+    let items = buscarNombre(arrProdFijo, inputBuscar.value);
+    productosDin.innerHTML = "";
+    for (const item of items) {
+
+        productosDin.innerHTML += crearCard(item);
+    }
+    agregarListener();
+})
+
+// muestra items en listado //
+function iniciarItems() {
+    productosDin.innerHTML = "";
+    for (const item of arrProdFijo) {
+
+        productosDin.innerHTML += crearCard(item);
+    }
+    agregarListener();
+}
+
+// evento del boton agregar // 
+
+eventoBoton = function (e) {
+    let found = buscarId(arrProdFijo, e.target.value);
+    arrCarrito.push(found);
+    let carrito = { items: arrCarrito, };
+    localStorage.setItem("carrito", JSON.stringify(carrito));
+    Swal.fire({
+        titleText: 'item agregado al carro',
+        icon: 'success',
+        background: '#F1F5F2',
+    })
+};
+
+// agrego listener a todos los botones //
+
+function agregarListener() {
+    let botonesAgregar = document.getElementsByClassName('agregar');
+    for (const boton of botonesAgregar) {
+        boton.addEventListener('click', eventoBoton);
+
+    }
+}
+
+// buscar item por id //
+const buscarId = (arr, filtro) => {
+    const buscar = arr.find((el) => {
+        return el.id == filtro
+    })
+    return buscar
+}
+
+iniciarItems();
